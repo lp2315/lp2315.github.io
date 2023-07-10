7c768b07d82486bc1cac9adf40b21fd4a6dab92f
din0o <a05iaojfw@mozmail.com>
2023-07-10 20:32:12 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 
 function shuffleColor() {
-    document.getElementById("nav").style.backgroundColor="#C4B198";
+    document.getElementById("navsection").style.backgroundColor="#C4B198";
 }
 
 const myHeading = document.querySelector("h1");
