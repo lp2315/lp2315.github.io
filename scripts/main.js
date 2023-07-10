3e3bd1e8d37517357cd665a1bf25b2013548d475
din0o <a05iaojfw@mozmail.com>
2023-07-10 22:19:43 +0200

    more fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,28 @@
 
+function currentTime() {
+    let date = new Date();
+    let day = date.getDay();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+    ss = (ss < 10) ? "0" + ss : ss;
 
-function buttonColor() {
-    document.getElementById("foot").backgroundColor="#2E4155";
+    let time = hh + ":" + mm + ":" + ss + " ";
+
+    document.getElementById("clock").innerText = time;
+    document.getElementById("day").innerText = day;
+    var t = setTimeout(function(){ currentTime() }, 1000);
+
 }
 
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Boob!";
+currentTime();
+
+
+
+
+function buttonColor() {
+    document.getElementById("header").style.color="#2E4155";
+}
+
