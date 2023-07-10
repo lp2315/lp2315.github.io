d90feead7bf4c864a6fad6b19012dc1915d2e424
din0o <a05iaojfw@mozmail.com>
2023-07-10 20:25:35 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 
 function buttonColor() {
-    document.getElementById("nav").style.background="#C4B198";
+    document.getElementById("footer").style.background="#C4B198";
 }
 
 const myHeading = document.querySelector("h1");
