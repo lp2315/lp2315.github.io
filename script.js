03ee4cdbb366f60adab4eb767ea7804afc072709
din0o <a05iaojfw@mozmail.com>
2024-10-18 17:53:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('nav ul li a');
     const sections = document.querySelectorAll('main section');
 
-    // Initial display setup
-    sections.forEach(section => {
-        section.style.display = 'none';
-    });
-    if (window.location.hash) {
-        const initialTargetId = window.location.hash.substring(1);
-        const initialTargetSection = document.getElementById(initialTargetId);
-        if (initialTargetSection) {
-            initialTargetSection.style.display = 'block';
-        }
+    // Function to show the desired section
+    function showSection(section) {
+        fetch(`${section}.html`)
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('main-content').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading section:', error));
+    }
+
+    // Check the current hash and set the section
+    const currentHash = window.location.hash.substring(1);
+    if (currentHash) {
+        showSection(currentHash);
     } else {
-        sections[0].style.display = 'block'; // Show the first section by default
+        showSection('home'); // Default start section
     }
 
     // Navigation click handling
@@ -22,28 +26,13 @@ document.addEventListener('DOMContentLoaded', function() {
             event.preventDefault();
             const targetId = this.getAttribute('href').substring(1);
             window.location.hash = targetId;
-            sections.forEach(section => {
-                section.style.display = section.id === targetId ? 'block' : 'none';
-            });
+            showSection(targetId);
         });
     });
 
     // Hash change handling
     window.addEventListener('hashchange', function() {
         const targetId = window.location.hash.substring(1);
-        sections.forEach(section => {
-            section.style.display = section.id === targetId ? 'block' : 'none';
-        });
+        showSection(targetId);
     });
 });
-
-// retrieves section value for section key,
-// changes the attribute of document to section
-function showSection(section) {
-    fetch(`${section}.html`)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('main-content').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading section:', error));
-}
